dd6f7f80016d3dd61ee028c4f079ad7a40952d9f
Antoine <contact@chess-ma2.com>
2021-03-05 21:58:15 +0100

    After a long conf call with marie add few others base of structure.

--- src/common/c/rules/pieces.h
+++ src/common/c/rules/pieces.h
@@ -14,7 +14,7 @@ Start the 2021-03-04
 
 
 //Possibility of the type of a Piece
-enum Type  
+enum Type
 {
     NONE = 0,
     PAWN,
@@ -33,5 +33,19 @@ enum Color
 	    WHITE = 1,
 };
 
+
+/**
+ * @author Antoine
+ * @date Start 05/03/2021
+ * @details Structure who represent a Piece.
+ * @param type represent with enum Type the type of Piece.
+ * @param color represent the color of the piece,
+ *      please ignore this last arg if type is None.
+ */
+struct Piece
+{
+  enum Type type;
+  enum Color color;
+};
 
 #endif
--- src/common/c/rules/plate.h
+++ src/common/c/rules/plate.h
@@ -1,10 +1,12 @@
 #ifndef PLATE_H
 #define PLATE_H
+#include "pieces.h"
+
 
 /*
 
 File .h containing all the functions relating to the chess plate and structure about the game.
-By Marie Maturana
+By Marie Maturana & Antoine
 Start the 2021-03-04
 
 */
@@ -16,5 +18,14 @@ enum turn{
 	    BLACKTURN = 0,
 	    WHITETURN = 1,
 };
+
+
+/**
+ * @author Antoine
+ * @date Start 05/03/2021
+ * @details Prototype of function who allow to generate a new grid.
+ * @return board of size 64 who represent each Piece in 8*8
+ */
+struct Piece *init_board (); 
 
 #endif
