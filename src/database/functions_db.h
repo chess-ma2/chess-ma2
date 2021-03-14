e681f343cee69a8bbdaa0ac1bcab47770e03e30a
Marine Thunet <marine.thunet@epita.fr>
2021-03-14 16:43:21 +0100

    lite db update function and supression done

--- src/database/functions_db.h
+++ src/database/functions_db.h
@@ -18,4 +18,19 @@ int rightPassword(char *email, unsigned char password[64]);
 //Get Id from mail
 size_t getID(char *email);
 
+//Adding one point of victory to user
+void update_victory(char * email);
+
+//Adding one point of lose to user
+void update_loss( char * email);
+
+//Changing name of user
+void update_name( char * email, char * new);
+
+//Changing mail of user
+void update_email( char * email, char * new);
+
+//Deleting user form database
+void delete_user(char * email);
+
 #endif
