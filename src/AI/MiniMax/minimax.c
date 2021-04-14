ca73f43a778d438a4c25227a3ade1fa1247da695
Anna Barry <anna.barry@epita.fr>
2021-04-14 11:28:07 +0100

    adding tables needed for minimax

---
+++ src/AI/MiniMax/minimax.c
@@ -0,0 +1,59 @@
+#include "minimax.h"
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+// Position evaluation function
+int getVal(struct Piece current)
+{
+  int res = 0; // Default is NULL
+  if(current.color == BLACK) // Piece is black
+  {
+      switch(current.type) // Set value from type
+      {
+          case PAWN:
+              res = -10;
+              break;
+          case ROOK:
+              res = -50;
+              break;
+          case BISHOP:
+              res = -30;
+              break;
+          case KNIGHT:
+              res = -30;
+              break;
+          case QUEEN:
+              res = -90;
+              break;
+          case KING:
+              res = -900;
+              break;
+          default: res = 0;
+      } }
+  else
+  {   switch(current.type)
+      {
+        case PAWN:
+            res = 10;
+            break;
+        case ROOK:
+            res = 50;
+            break;
+        case BISHOP:
+            res = 30;
+            break;
+        case KNIGHT:
+            res = 30;
+            break;
+        case QUEEN:
+            res = 90;
+            break;
+        case KING:
+            res = 900;
+            break;
+        default: res = 0;
+      }
+  }
+  return res;
+}
---
+++ src/AI/MiniMax/testing.c
@@ -0,0 +1,6 @@
+#include "minimax.h"
+
+int main()
+{
+  return 0;
+}
