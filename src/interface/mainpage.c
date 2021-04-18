611def1a507742b855d6021813c990f0a3dbc96b
Marine Thunet <marine.thunet@epita.fr>
2021-04-18 15:00:57 +0200

    reunited both gtk files, still need to add movement

--- src/interface/mainpage.c
+++ src/interface/mainpage.c
@@ -5,6 +5,7 @@
 #ifndef MAINPAGE_C
 #define MAINPAGE_C
 
+//_____INTITIALISATION___________________________________
 //__________ variables
 GtkWidget* window1;
 
@@ -17,10 +18,22 @@ GtkWidget* NewPL1_W1;
 GtkWidget* window1_version_PL2;
 // New player n2 first version
 GtkWidget* NewPL2_W1;
+//Login player 1
+GtkWidget* LoginAccount;
+//Login player 2
+GtkWidget* LoginAccount2;
 
 //____ GAME ____________
 // Main Page for GAME
 GtkWidget* game_v1;
+
+
+//____Global_Initialization___
+// The player structure
+struct Player *pl1;
+// The player structure
+struct Player *pl2;
+
 
 //______ First Player ________
 // Entry for name
@@ -31,8 +44,6 @@ GtkEntry * Email_Entry1;
 GtkEntry * Password_Entry1;
 // Label to say if accound has been created with success
 GtkLabel * Create_account1_yes;
-// The player structure
-struct Player *pl1;
 
 //_________ Transition ___________________________
 // Transition Window
@@ -47,8 +58,23 @@ GtkEntry * Email_Entry2;
 GtkEntry * Password_Entry2;
 // Label to say if accound has been created with success
 GtkLabel * Create_account2_yes;
-// The player structure
-struct Player *pl2;
+
+//___Login 1_________________
+
+//Entry for Email
+GtkEntry * Email_Log1;
+//Entry for Password
+GtkEntry * Password_Log1;
+//Reusing structure player 1 or 2 depending
+
+
+//___Login 2_________________
+
+//Entry for Email
+GtkEntry * Email_Log2;
+//Entry for Password
+GtkEntry * Password_Log2;
+//Reusing structure player 1 or 2 depending
 
 //____________________ Second Window
 // second version window
@@ -58,6 +84,11 @@ GtkWidget* window2nd_v_1;
 // third version window
 GtkWidget* window3rd_v_1;
 
+
+//__________________________________________________________________
+//END_____INITIALISATION____________________________________________
+//__________________________________________________________________
+
 /*
  * @author Anna
  * @date 10/04/2021
@@ -66,6 +97,7 @@ GtkWidget* window3rd_v_1;
 void first_v_start(GtkButton *button, gpointer user_data)
 { gtk_widget_show(window1st_v_1);
   gtk_widget_hide(window1);         }
+
 
 /*
   * @author Anna
@@ -176,6 +208,54 @@ void save_pl2(GtkButton *button, gpointer user_data)
 
 //____________________________________________________________________________
 /*
+ * @author Marine
+ * @date 16/04/2021
+ * @details Show the login1 window
+ */
+void login1_start(GtkButton *button, gpointer user_data)
+{
+    gtk_widget_show(LoginAccount);
+    gtk_widget_hide(window1);
+}
+
+ /*
+  * @author Marine
+  * @date 16/04/2021
+  * @details Back to the mainpage from login1
+  */
+void back_from_login1(GtkButton *button, gpointer user_data)
+{
+    gtk_widget_show(window1);
+    gtk_widget_hide(LoginAccount);
+}
+
+//____________________________________________________________________________
+/*
+ * @author Marine
+ * @date 16/04/2021
+ * @details Show the login2 window
+ */
+void login2_start(GtkButton *button, gpointer user_data)
+{
+    gtk_widget_show(LoginAccount2);
+    gtk_widget_hide(window1);
+}
+
+ /*
+  * @author Marine
+  * @date 16/04/2021
+  * @details Back to the mainpage from login2
+  */
+void back_from_login2(GtkButton *button, gpointer user_data)
+{
+    gtk_widget_show(window1);
+    gtk_widget_hide(LoginAccount2);
+}
+
+
+
+//____________________________________________________________________________
+/*
  * @author Anna
  * @date 10/04/2021
  * @details Show second version's window
@@ -261,10 +341,14 @@ int main (int argc, char *argv[])
 
     // Button to go back to main page
     GtkButton* back = GTK_BUTTON(gtk_builder_get_object(builder, "back"));
+    GtkButton* back3 = GTK_BUTTON(gtk_builder_get_object(builder,"back3"));
 
     // New Player n1 ___________________________________________________________________
     // New Player n°1 button
+    //create
     GtkButton* new_player1 = GTK_BUTTON(gtk_builder_get_object(builder, "new_player1"));
+    //login
+    GtkButton* login1 = GTK_BUTTON(gtk_builder_get_object(builder,"login_1"));
 
     // ButtonImage to go back to mainpage
     GtkWidget* Image_back;
@@ -275,24 +359,34 @@ int main (int argc, char *argv[])
 
     // First player is a new player Window
     NewPL1_W1 = GTK_WIDGET(gtk_builder_get_object(builder, "NewPL1_W1"));
+    // First player exists
+    LoginAccount = GTK_WIDGET(gtk_builder_get_object(builder, "LoginAccount"));
 
     // Back to first version
     GtkButton* back_w1 = GTK_BUTTON(gtk_builder_get_object(builder, "back_w1"));
+    GtkButton* back_w3 = GTK_BUTTON(gtk_builder_get_object(builder,"back_w3"));
 
     // ButtonImage to go back to first version window
     GtkWidget* Image_back2;
     Image_back2 = gtk_image_new_from_file ("Images/back.png");
     GtkWidget* Image_back2_bis;
     Image_back2_bis = gtk_image_new_from_file ("Images/back.png");
+    
     gtk_button_set_image(back_w1, Image_back2_bis);
+    gtk_button_set_image(back_w3,Image_back2_bis);
 
     // Save new info about player 1
     GtkButton* lock_new1 = GTK_BUTTON(gtk_builder_get_object(builder, "lock_new1"));
+    GtkButton* lock_new3 = GTK_BUTTON(gtk_builder_get_object(builder, "lock_new3"));
+    
     GtkWidget* Image_save;
     GtkWidget* Image_savebis;
+    
     Image_save = gtk_image_new_from_file ("Images/save.png");
     Image_savebis = gtk_image_new_from_file ("Images/save.png");
+    
     gtk_button_set_image(lock_new1, Image_savebis);
+    gtk_button_set_image(lock_new3, Image_savebis); //tochange
 
     // Label when info is saved
     Create_account1_yes = GTK_LABEL(gtk_builder_get_object(builder, "create_account1_yes"));
@@ -304,6 +398,12 @@ int main (int argc, char *argv[])
     // Entry for password
     Password_Entry1 = GTK_ENTRY(gtk_builder_get_object(builder, "password1"));
 
+    //LOGIN ACCOUNT
+    //entry for mail
+    Email_Log1 = GTK_ENTRY(gtk_builder_get_object(builder,"email3"));
+    //entry for password
+    Password_Log1= GTK_ENTRY(gtk_builder_get_object(builder,"password3"));
+    
     // Transition ______________________________________________________________________
     // Window
     transition1 = GTK_WIDGET(gtk_builder_get_object(builder, "transition1"));
@@ -322,22 +422,30 @@ int main (int argc, char *argv[])
     // New Player n2 ___________________________________________________________________
     // New Player n°2 button
     GtkButton* new_player2 = GTK_BUTTON(gtk_builder_get_object(builder, "new_player2"));
+    GtkButton* login_2 = GTK_BUTTON(gtk_builder_get_object(builder, "login_2"));
 
     // ButtonImage to go back to mainpage
     gtk_button_set_image(back2, Image_back);
 
     // First player is a new player Window
     NewPL2_W1 = GTK_WIDGET(gtk_builder_get_object(builder, "NewPL2_W1"));
+    //Second player is not new
+    LoginAccount2=GTK_WIDGET(gtk_builder_get_object(builder, "LoginAccount2"));
 
     // Back to first version
     GtkButton* back_w2 = GTK_BUTTON(gtk_builder_get_object(builder, "back_w2"));
+    //Second player is not new
+    LoginAccount2=GTK_WIDGET(gtk_builder_get_object(builder, "LoginAccount2"));
 
     // Button to go back to first version window
     gtk_button_set_image(back_w2, Image_back2);
+    gtk_button_set_image(back_w4, Image_back2);
 
     // Save new info about player 2
     GtkButton* lock_new2 = GTK_BUTTON(gtk_builder_get_object(builder, "lock_new2"));
     gtk_button_set_image(lock_new2, Image_save);
+    GtkButton* lock_new4 = GTK_BUTTON(gtk_builder_get_object(builder, "lock_new4"));
+    gtk_button_set_image(lock_new4,Image_save); //tochange
 
     // Label when info is saved
     Create_account2_yes = GTK_LABEL(gtk_builder_get_object(builder, "create_account2_yes"));
@@ -348,6 +456,12 @@ int main (int argc, char *argv[])
     Email_Entry2 = GTK_ENTRY(gtk_builder_get_object(builder, "email2"));
     // Entry for password
     Password_Entry2 = GTK_ENTRY(gtk_builder_get_object(builder, "password2"));
+    
+    //LOGIN ACCOUNT
+   //entry for mail
+   Email_Log2 = GTK_ENTRY(gtk_builder_get_object(builder,"email4"));
+   //entry for password
+   Password_Log2= GTK_ENTRY(gtk_builder_get_object(builder,"password4"));
 
     //  Game  _______________________________________________________________________
     GtkDrawingArea* area = GTK_DRAWING_AREA(gtk_builder_get_object(builder, "area"));
