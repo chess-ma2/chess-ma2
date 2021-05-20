c8433b63fe5c960f54ccda83cf638a52e1497012
Marine Thunet <marine.thunet@epita.fr>
2021-05-20 08:31:46 +0200

    avancées rapides just je push juste pour gérer le merge

--- src/AI/MiniMax/minimax.c
+++ src/AI/MiniMax/minimax.c
@@ -8,7 +8,7 @@
 #ifndef MINIMAX_C
 #define MINIMAX_C
 
-
+//QUESTIONS A POSER A ANNA pour la finalisation et les tests
 int get_min_from_list(int nb_children,struct node *children)
 {
     int min = children[nb_children-1].score;
@@ -39,6 +39,40 @@ int get_max_from_list(int nb_children,struct node *children)
     return max;
 }
 
+struct finalmove * get_move (struct tree * T)
+{
+    struct node * root = T->root;
+    int color = root->MiniMax;
+    int nb_children= root->nb_children;
+    int idxmove=nb_children;
+    
+    if (color==1)//1 =MINI? si oui faire ca
+    {
+        nb_children--;
+        while (nb_children>=0)
+        {
+            if (children[nb_children].score < children[idxmove].score)
+            {
+                idxmove= children[nb_children].score;
+            }
+            nb_children--;
+        }
+    }
+    else
+    {
+        nb_children--;
+        while (nb_children>=0)
+        {
+            if (children[nb_children].score > children[max].score)
+            {
+                max = children[nb_children].score;
+            }
+            nb_children--;
+        }
+    }
+    
+}
+
 struct finalmove * get_right_move_ia(struct Piece *board, enum turn player_turn, int nb_List, int depth)
 {
     struct finalmove * final = malloc(sizeof(struct finalmove));
@@ -47,20 +81,35 @@ struct finalmove * get_right_move_ia(struct Piece *board, enum turn player_turn,
 
     //struct tree * T = create_tree(board, player_turn, current_List, nb_List, current_ListB, 16, depth);
     //struct node * root = T->root;
-    return final;
+    T= update_values(T,1);
+    struct finalmove * move = get_move(T);
+    return move;
 }
 
-struct tree * update_values(struct tree * T, int color, int depth)
+struct tree * update_values(struct tree * T, int color)
 {
     struct node * root = T->root;
+    for (int i=0; i<root->nb_children; i++)
+    {
+        if (nb_children>0)
+        {
+            root = update_values(root,color);
+            //ALPHABETA
+            //si mini et valeur maxi alors virer 
+        }
+    }
     if (color==BLACK)
     {
-        get_min_from_list(root->nb_children, root->children);
+        root.score = get_min_from_list(root->nb_children, root->children);
     }
-    else{
-        get_max_from_list(root->nb_children,root->children);
+    else
+    {
+        root.score = get_max_from_list(root->nb_children,root->children);
     }
     return T;
 }
+//essai de alpha beta
+//NOTE IL FAUT AJOUTER AU NOEUDS DES VALEURS MAX ET MIN
+
 
 #endif
